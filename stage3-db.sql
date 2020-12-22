4a2be4b9966d85615363ad6a37d17df61a61fced
jsolina <jcsolina.itd.pps@gmail.com>
2020-12-22 09:57:03 +0800

    Removed: Removed folder named: stage3-api(orig) - nailagay ko lng itong folder na ito at naisama sa push Updated: Stage3 database backup updated

--- stage3-db.sql
+++ stage3-db.sql
@@ -1,69 +1,82 @@
-/*
-SQLyog Ultimate v12.4.1 (64 bit)
-MySQL - 10.4.14-MariaDB : Database - todo
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: todo
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `todo`;
-
-/*Table structure for table `itemlist` */
+--
+-- Table structure for table `itemlist`
+--
 
 DROP TABLE IF EXISTS `itemlist`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `itemlist` (
-  `idItem` int(45) NOT NULL AUTO_INCREMENT,
-  `idTask` int(45) DEFAULT NULL,
+  `idItem` int NOT NULL AUTO_INCREMENT,
+  `idTask` int DEFAULT NULL,
   `itemName` varchar(65) DEFAULT NULL,
   `itemDetails` varchar(65) DEFAULT NULL,
   `itemStatus` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idItem`),
   KEY `idTask` (`idTask`),
   CONSTRAINT `itemlist_ibfk_1` FOREIGN KEY (`idTask`) REFERENCES `tasklist` (`idTask`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `itemlist` */
+--
+-- Dumping data for table `itemlist`
+--
 
-insert  into `itemlist`(`idItem`,`idTask`,`itemName`,`itemDetails`,`itemStatus`) values 
-(1,1,'item1','123',NULL),
-(2,1,'item2','321',NULL),
-(3,2,'czczz','czz',NULL);
+LOCK TABLES `itemlist` WRITE;
+/*!40000 ALTER TABLE `itemlist` DISABLE KEYS */;
+INSERT INTO `itemlist` VALUES (1,1,'item1','123','Pending'),(2,1,'item2','321','Done'),(3,2,'czczz','czz',NULL),(4,1,'item3..','item3 desc','Pending');
+/*!40000 ALTER TABLE `itemlist` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `tasklist` */
+--
+-- Table structure for table `tasklist`
+--
 
 DROP TABLE IF EXISTS `tasklist`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasklist` (
-  `idTask` int(45) NOT NULL AUTO_INCREMENT,
+  `idTask` int NOT NULL AUTO_INCREMENT,
   `taskName` varchar(65) DEFAULT NULL,
   `taskDetails` varchar(65) DEFAULT NULL,
+  `email` varchar(85) DEFAULT NULL,
   PRIMARY KEY (`idTask`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `tasklist` */
+--
+-- Dumping data for table `tasklist`
+--
 
-insert  into `tasklist`(`idTask`,`taskName`,`taskDetails`) values 
-(1,'task1','taskDetails3'),
-(2,'task2','taskDetails2'),
-(3,'task3','taskDetails3'),
-(10,'task4.','taskDeails4...'),
-(11,'22222','222222'),
-(12,'ew','awe'),
-(13,'111','11111'),
-(14,'14aa','14124'),
-(15,'hhhh','hh'),
-(16,'jajaja','jujujuj');
+LOCK TABLES `tasklist` WRITE;
+/*!40000 ALTER TABLE `tasklist` DISABLE KEYS */;
+INSERT INTO `tasklist` VALUES (1,'task1','taskDetails1','task1@gmail.com'),(2,'task2','taskDetails2','task2@gmail.com'),(3,'task3','taskDetails3','task3@gmail.com');
+/*!40000 ALTER TABLE `tasklist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-22  9:53:00
